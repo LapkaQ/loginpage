8608e44878a0e3e4289a81fa86bf9e4ad6871c01
lapkaQ <wojtekgaw2007@gmail.com>
2024-03-08 18:05:39 +0100

    add form

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,21 +1,66 @@
+import React, { useState } from "react";
 import "./MainForm.css";
 const MainForm = () => {
+  const [activeForm, setActive] = useState(false);
   return (
     <form className="form">
-      <h1>Login</h1>
-      <div className="loginInputs">
-        <label>Username</label>
-        <input type="text" id="UsernameInput" />
+      <div class={activeForm ? "loginForm" : "loginForm activeLoginForm"}>
+        <h1>Login</h1>
+        <div className="chooseMethod">
+          <button
+            className="formButton active"
+            onClick={() => setActive(false)}
+          >
+            Login
+          </button>
+          <button className="formButton" onClick={() => setActive(true)}>
+            Singup
+          </button>
+        </div>
+        <div className="inputsForm">
+          <div className="inputForm">
+            <input type="text" placeholder="Username" required />
+            <span class="material-symbols-outlined inputIcon">person</span>
+          </div>
+          <div className="inputForm">
+            <input type="password" placeholder="Password" required />
+            <span class="material-symbols-outlined inputIcon">lock</span>
+          </div>
+          <a href="" id="forgot">
+            Forgot password?
+          </a>
+        </div>
+
+        <button className="formButton active">Login</button>
       </div>
-      <div className="loginInputs">
-        <label>Password</label>
-        <input type="password" id="PasswordInput" />
+
+      <div class={activeForm ? "loginForm activeRegisterForm" : "loginForm"}>
+        <h1>Register</h1>
+        <div className="chooseMethod">
+          <button className="formButton" onClick={() => setActive(false)}>
+            Login
+          </button>
+          <button className="formButton active" onClick={() => setActive(true)}>
+            Singup
+          </button>
+        </div>
+        <div className="inputsForm">
+          <div className="inputForm">
+            <input type="text" placeholder="Username" required />
+            <span class="material-symbols-outlined inputIcon">person</span>
+          </div>
+          <div className="inputForm">
+            <input type="text" placeholder="Email" required />
+            <span class="material-symbols-outlined inputIcon">mail</span>
+          </div>
+          <div className="inputForm">
+            <input type="password" placeholder="Password" required />
+            <span class="material-symbols-outlined inputIcon">lock</span>
+          </div>
+        </div>
+
+        <button className="formButton active">Singup</button>
       </div>
-      <a href="" id="forgotPassword">
-        Forgot Password?
-      </a>
-      <button id="SignUp">Sign up</button>
-      <div id=""></div>
     </form>
   );
 };
